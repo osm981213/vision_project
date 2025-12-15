2778cf6205333839617cc8a837e085fef54cce26
KeyboardBK <gkwnsgur1234@naver.com>
2025-12-15 17:01:40 +0900

    플렛폼 최초 커밋

---
+++ platform/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import CCTVTracker from "./pages/CCTVTracker";
+
+function App() {
+  return <CCTVTracker />;
+}
+
+export default App
